09cf5364d22d2700d3bee4f80b2a2fe5b7267273
MirellaLins <mirella.lds@gmail.com>
2019-05-25 19:06:35 -0300

    Initialize project

---
+++ ToDoList/ToDoList/ToDoList/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using SQLite;
+using System;
+using System.IO;
+using ToDoList.models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoList
+{
+    public partial class App : Application
+    {
+        private static SQLiteConnection database;
+
+        public static SQLiteConnection DB
+        {
+            get
+            {
+                if (database == null)
+                {
+                    string dbName = "toDoList.db3";
+                    string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                    database = new SQLiteConnection(Path.Combine(path, dbName));
+                    database.CreateTable<Task>();
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ToDoList/ToDoList/ToDoList/models/Task.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.models
+{
+    [Table("Task")]
+    class Task
+    {
+        [PrimaryKey(), AutoIncrement()]
+        public int Id { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("endDate")]
+        public string EndDate { get; set; }
+    }
+}
