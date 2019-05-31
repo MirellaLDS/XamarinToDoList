9b02881d13905a51cf06b8552f5020ab0d3bf363
Eriberto Junior <eriberto.brasileiro@gmail.com>
2019-05-30 22:38:23 -0300

    Aplicando título na tela inicial que sabe quando o usuário está acessando o app pela primeira vez

--- ToDoList/ToDoList/ToDoList/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ ToDoList/ToDoList/ToDoList/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,6 +17,9 @@ namespace ToDoList {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label lbTitle;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private global::Xamarin.Forms.Label OpenTask;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
@@ -25,6 +28,7 @@ namespace ToDoList {
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            lbTitle = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "lbTitle");
             OpenTask = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "OpenTask");
             listaContatos = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "listaContatos");
         }
