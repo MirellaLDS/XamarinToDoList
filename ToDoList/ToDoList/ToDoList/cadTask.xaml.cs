198c94806b488aff2d2d31a3387d1f2db2e8498f
MirellaLins <mirella.lds@gmail.com>
2019-05-30 22:38:12 -0300

    properts show

--- ToDoList/ToDoList/ToDoList/cadTask.xaml.cs
+++ ToDoList/ToDoList/ToDoList/cadTask.xaml.cs
@@ -26,7 +26,7 @@ namespace ToDoList
                 Description = description.Text,
                 EndDate = data
             };
-
+            limparDados();
             App.DB.Insert(newTask);
             DisplayAlert("Cadastrar", "Cadastrado com sucesso!", "OK");
             Navigation.PopAsync();
@@ -41,19 +41,32 @@ namespace ToDoList
         void changeTextoTitle(object sender, TextChangedEventArgs e)
         {
             saveInstantState();
-
         }
 
         void changeTextoDescription(object sender, TextChangedEventArgs e)
         {
             saveInstantState();
+        }
 
+        protected override void OnAppearing()
+        {
+            String titleApp = Application.Current.Properties["Title"].ToString();
+            if (titleApp != null)
+            {
+                title.Text = titleApp;
+            }
+            String descriptionApp = Application.Current.Properties["Description"].ToString();
+            if (descriptionApp != null)
+            {
+                description.Text = descriptionApp;
+            }
+            
         }
 
         void saveInstantState()
         {           
-            Application.Current.Properties["Title"] = title.Text;
-            Application.Current.Properties["Description"] = description.Text;
+            Application.Current.Properties["Title"] = title.Text == null ? "" : title.Text;
+            Application.Current.Properties["Description"] = description.Text == null ? "" : description.Text;
             Application.Current.Properties["EndDate"] = data;
 
             Application.Current.SavePropertiesAsync();
@@ -62,9 +75,9 @@ namespace ToDoList
 
         void limparDados()
         {
-            Application.Current.Properties["Title"] = null;
-            Application.Current.Properties["Description"] = null;
-            Application.Current.Properties["EndDate"] = null;
+            Application.Current.Properties["Title"] = "";
+            Application.Current.Properties["Description"] = "";
+            Application.Current.Properties["EndDate"] = "";
         }
 
     }
